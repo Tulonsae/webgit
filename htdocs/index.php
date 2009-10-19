8269ee5f6e4710b1ab2f6f1129d6d7d4bd4f19e1
felix <felix@seconddrawer.com.au>
2009-10-20 00:39:03 +0700

    basic cleanups

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,14 +8,14 @@ require(dirname(__FILE__) . './../include/config.php');
 // /project/object/id
 
 // got a project
-if(isset($_GET['p'])){
-  $project = get_repo_info($config['project_root'] . '/' . $_GET['p']);
+if(isset($_GET['p']) && file_exists($config['project_root'] . '/' . $_GET['p'])){
+  $project = get_project_info($config['project_root'] . '/' . $_GET['p']);
   print_r($project);
 
 }else{ // no project, summary
   $projects = array();
   foreach(array_diff(scandir($config['project_root']), array('.','..')) as $project_dir) {
-    if($project = get_repo_info($config['project_root'] . '/' . $project_dir)){
+    if($project = get_project_info($config['project_root'] . '/' . $project_dir)){
       $projects[] = $project;
     }
   }
--- include/functions.php
+++ include/functions.php
@@ -1,29 +1,57 @@
 <?php
 
-function get_repo_info($path) {
+function get_project_info($path) {
   global $config;
 
   $project = null;
 
   // valid repo TODO
   if(file_exists($path . '/HEAD')){
-    
-    $project['visible'] = true;
-    // check publishing status
-    if($config['export_ok']) {
-      if(!file_exists($path . '/git-daemon-export-ok')) {
-        if($config['strict_export']) {
-          // not available
-          return null;
-        }
-        // available but not visible
+
+    if(project_is_available($path)) {
+      if(project_is_visible($path)) {
+        $project['visible'] = true;
+      } else {
         $project['visible'] = false;
       }
+      $project['git'] = new Git($path);
+      $project['name'] = basename($path);
+      if(file_exists($path . '/description')){
+        $project['description'] = file_get_contents($path . '/description');
+      }
     }
-    $project['git'] = new Git($path);
-    $project['name'] = basename($path);
-    if(file_exists($path . '/description')){
-      $project['description'] = file_get_contents($path . '/description');
+  }
+  return $project;
+}
+
+function project_is_visible($path) {
+  global $config;
+
+  if(!project_is_available($path) || 
+    ($config['export_ok'] && !file_exists($path . '/git-daemon-export-ok'))) {
+      return false;
+    }
+  return true;
+}
+
+function project_is_available($path) {
+  global $config;
+
+  if($config['strict_export'] && !file_exists($path . '/git-daemon-export-ok')) {
+    return false;
+  }
+  return true;
+}
+
+function get_project_list() {
+  global $config;
+
+  $projects = array();
+
+  if(isset($config['repo_list'])) {
+  } else {
+    foreach(array_diff(scandir($config['project_root']), array('.','..')) as $project_dir) {
+      $projects[] = $project_dir;
     }
   }
   return $project;
